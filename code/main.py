9e793948d1a58aab9a952176e1a42c8f09131503
Jacob Brott <brott022@umn.edu>
2023-11-03 13:36:49 -0500

    Network 1 Done

--- code/main.py
+++ code/main.py
@@ -30,43 +30,32 @@ def forward_pass(network, input_data):
     layer_input = input_data
     for layer in network['Layers']:
         weights = layer['weights']
-        biases = layer['biases'][0]  # Adjusting this line to access the first row of biases matrix
+        biases = layer['biases']
         layer_input = matmul([layer_input], weights)[0]
         layer_input = add_vectors(layer_input, biases)
         if layer['relu']:
-            layer_input = apply_activation(relu, [layer_input])[0]  # Adjusting this line to treat layer_input as a matrix
+            layer_input = [relu(x) for x in layer_input]
     return layer_input
 
 # Parse networks from file
 def parse_networks(filename):
     networks = []
     current_network = None
-    current_layer = None
     
     with open(filename, 'r') as f:
         for line in f:
             parts = line.strip().split(':')
-            key = parts[0].strip()
-            value = ':'.join(parts[1:]).strip()
-            
-            if key == 'Layers':
-                if current_network is not None:
-                    networks.append(current_network)
+            if parts[0].startswith('Layers'):
                 current_network = {'Layers': []}
-            elif key.startswith('Rows') or key.startswith('Cols'):
-                pass  # We can infer Rows and Cols from Weights matrix
-            elif key.startswith('Weights') or key.startswith('Biases'):
-                matrix = eval(value)
-                current_layer[key.lower()] = matrix
-            elif key == 'Relu':
-                current_layer['relu'] = value.lower() == 'true'
-            elif key.startswith('Example_Input') or key.startswith('Example_Output'):
-                pass  # These are not used for network definition
-            else:
-                current_layer = {}
+                networks.append(current_network)
+            elif parts[0].startswith('Weights'):
+                current_layer = {'weights': eval(parts[1].strip())}
                 current_network['Layers'].append(current_layer)
-                
-        networks.append(current_network)  # Add the last network
+            elif parts[0].startswith('Biases'):
+                current_layer['biases'] = eval(parts[1].strip())[0]  # Flatten the biases list
+            elif parts[0].startswith('Relu'):
+                current_layer['relu'] = parts[1].strip().lower() == 'true'
+    
     return networks
 
 # Function to calculate the sum of absolute values
@@ -74,14 +63,14 @@ def sum_of_abs(values):
     return sum(abs(x) for x in values)
 
 # Optimization function to minimize the output of the neural network
-def optimize(network, num_iterations=10000, learning_rate=0.01):
-    input_size = len(network['Layers'][0]['weights'][0])  # Adjusting this line to access the first row of weights matrix
+def optimize(network, num_iterations=10000, step_size=0.01):
+    input_size = len(network['Layers'][0]['weights'])
     best_input = [random.uniform(-1, 1) for _ in range(input_size)]
     best_output = forward_pass(network, best_input)
     best_score = sum_of_abs(best_output)
     
     for _ in range(num_iterations):
-        candidate_input = [x + random.uniform(-learning_rate, learning_rate) for x in best_input]
+        candidate_input = [x + random.uniform(-step_size, step_size) for x in best_input]
         candidate_output = forward_pass(network, candidate_input)
         candidate_score = sum_of_abs(candidate_output)
         if candidate_score < best_score:
@@ -92,14 +81,18 @@ def optimize(network, num_iterations=10000, learning_rate=0.01):
 # Main function to execute the program
 def main():
     networks = parse_networks('networks.txt')
-    for i, network in enumerate(networks, 1):
-        print(f'Optimizing Network {i}')
-        optimized_input = optimize(network)
-        print('Optimized Input:', optimized_input)
-        optimized_output = forward_pass(network, optimized_input)
-        print('Network Output:', optimized_output)
-        print('Sum of Absolute Values:', sum_of_abs(optimized_output))
-        print('=' * 30)
+    with open('solutions.txt', 'w') as f_out:
+        for i, network in enumerate(networks):
+            print(f'Optimizing Network {i+1}')
+            optimized_input = optimize(network)
+            print('Optimized Input:', optimized_input)
+            optimized_output = forward_pass(network, optimized_input)
+            print('Network Output:', optimized_output)
+            print('Sum of Absolute Values:', sum_of_abs(optimized_output))
+            print('=' * 30)
+            
+            # Write the optimized input to the solutions.txt file
+            f_out.write(','.join(map(str, optimized_input)) + '\n')
 
 if __name__ == "__main__":
     main()
